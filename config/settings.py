2b9e628a81806663a2f41c15f2411638bd50750c
sharshenaliev <59019588+sharshenaliev@users.noreply.github.com>
2025-03-23 13:36:23 +0600

    server

--- config/settings.py
+++ config/settings.py
@@ -34,13 +34,14 @@ ALLOWED_HOSTS = ["*", ]
 # Application definition
 
 INSTALLED_APPS = [
+    'daphne',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'ninja_jwt',
+
     'accounts',
 ]
 
@@ -73,7 +74,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'config.wsgi.application'
-
+ASGI_APPLICATION = "config.asgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
